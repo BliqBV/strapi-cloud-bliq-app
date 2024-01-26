a0b09f21d66412cdf41bdc89c682cf019c722a9b
srgsrn <srnsergey@gmail.com>
2024-01-26 14:43:48 +0200

    new pages

--- src/api/my-subscription/routes/my-subscription.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * my-subscription router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::my-subscription.my-subscription');
--- src/api/my-subscription/services/my-subscription.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * my-subscription service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::my-subscription.my-subscription');
