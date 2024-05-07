8bdc64d9ea513cb6020304e473f8f8ae0fc72bab
Robert Vliek | SC <robert.vliek@solarclarity.com>
2024-05-07 15:33:54 +0200

    [B20-6136] Add CollectionType for subscriptions and single type for bliq powerflow page

---
+++ src/api/powerflow-subscription/services/powerflow-subscription.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * powerflow-subscription service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::powerflow-subscription.powerflow-subscription');
---
+++ src/api/subscription-powerflow/routes/subscription-powerflow.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * subscription-powerflow router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::subscription-powerflow.subscription-powerflow');
