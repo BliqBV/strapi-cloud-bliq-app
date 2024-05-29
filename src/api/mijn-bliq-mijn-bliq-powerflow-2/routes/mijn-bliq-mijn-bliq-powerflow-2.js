d6bd3115f67b0f439b979b77987519aad0c9d31e
srgsrn <srnsergey@gmail.com>
2024-05-29 12:45:31 +0300

    added temp "bliq-powerflow" page for testing

---
+++ src/api/mijn-bliq-mijn-bliq-powerflow-2/routes/mijn-bliq-mijn-bliq-powerflow-2.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mijn-bliq-mijn-bliq-powerflow-2 router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::mijn-bliq-mijn-bliq-powerflow-2.mijn-bliq-mijn-bliq-powerflow-2');
